90bc537c9ad2ee3f82cece84f6783fb52f83d606
mshakirkhan <mmshakirkhan@yahoo.com>
2019-11-07 17:30:07 +0500

    Create var-const.js

---
+++ const-let-var-example/var-const.js
@@ -0,0 +1,43 @@
+
+// In var base variable :
+
+// We can re-asign varibale 
+// We can also redefined 
+
+// Exapmle of re-asign varibale
+
+var myName = "Muhammad Shakir Khan";
+myName = "Khan Muhammad Shakir"
+console.log(myName);
+
+
+// Exapmle of redefined  varibale
+
+var myDesignation = "Full Stack Developer";
+var myDesignation = "MERN Stack Developer";
+console.log(myDesignation);
+
+// In Let base variable :
+
+// We can re-asign varibale
+// But We can't redefined 
+
+// Exapmle of re-asign varibale
+
+let myLocation = "Karachi, Pakistan";
+myLocation = "Lahore, Pakistan";
+console.log(myLocation);
+
+// Exapmle of redefined varibale, un comment below code and check the console it shows , ('myCountry' has already been declared), because we can't redefined varibale of let.
+
+// let myCountry = "Pakistan";
+// let myCountry = "UAE";
+// console.log(myCountry);
+
+// In const base variable :
+
+// We can't  re-asign varibale
+// Also We can't redefined varibale
+
+const myCity = "Karachi";
+console.log(myCity);
