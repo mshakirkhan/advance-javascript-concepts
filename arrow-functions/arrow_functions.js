609e70bb5568b8763d98e28eb0528c71919e8160
mshakirkhan <mmshakirkhan@yahoo.com>
2019-11-08 18:11:12 +0500

    Create arrow_functions.js

---
+++ arrow-functions/arrow_functions.js
@@ -0,0 +1,21 @@
+// ES5 Function Example
+
+const addition = function(value1, value2){
+	return value1 + value2;
+}
+
+console.log("Addition: " + addition(10,3));
+
+// Ans Will be 13
+
+// ES6 Arrow Function
+// Arrow function will always anonymous
+
+const multiplication = (value1, value2) => {
+	return value1 * value2;
+}
+
+console.log("Multiplication: " + multiplication(2,10));	
+
+// Ans Will be 20, this is example of arrow function
+
